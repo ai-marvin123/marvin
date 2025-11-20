c9ad137295e3f36067eeb9d4e7875901819b33f3
Mari Lee <marilee.vln@gmail.com>
2025-11-20 10:29:19 -0500

    added some files and folders

---
+++ client/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <>
+      <h1>Train like a top real estate agent!</h1>
+      <h2>Client Communication & Relationship Skill Builder</h2>
+    </>
+  );
+}
--- client/main.tsx
+++ client/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className='p-4'>
-      <h1 className='text-2xl font-bold mb-4 text-blue-800'>
-        Real Estate AI Coach
-      </h1>
-      <p>Frontend is running successfully!</p>
-    </div>
+    <App />
   </React.StrictMode>
 );
